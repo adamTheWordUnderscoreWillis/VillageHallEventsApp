d449ba4cb4b38277710247bb0cf614aec7fae314
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2025-01-09 16:25:12 +0000

    Tests getAllEvents endpoint and formats Events Data

--- server/db/models/events.Models.js
+++ server/db/models/events.Models.js
@@ -3,7 +3,21 @@ exports.fetchAllEvents = async ()=>{
     let db = database.getDb()
     let data = await db.collection("events").find({}).toArray()
     if(data.length>0){
-        return data
+        const formattedData = data.map((event)=>{
+            return {
+                name: event["name"],
+                id: event["_id"],
+                description: event["description"],
+                start: event["start"],
+                end: event["end"],
+                logo: event["logo"],
+                currancy: event["currency"],
+                created: event["created"],
+                
+            }
+        })
+        console.log(formattedData)
+        return formattedData
     }
     else{
         throw new Error("Data was not found")
