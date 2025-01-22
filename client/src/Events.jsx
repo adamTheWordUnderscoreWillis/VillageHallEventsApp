7c864c082e63a61bc99dfcef008d2dc21798f9ad
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2025-01-22 15:45:29 +0000

    Redesigns poster to show event text

--- client/src/Events.jsx
+++ client/src/Events.jsx
@@ -18,12 +18,12 @@ function Events() {
        return 0
       }
 
-      const posterImage = events[0].logo
+      // const posterImage = events[0].logo
       return (
         <>
              {events.map((event, index)=>{
                return (
-                   <Poster yRotation={(Math.random()-0.1)*0.5} xPosition={(index-4)*0.4} yPosition={(Math.random()-0.5)*2.5} zPosition={Math.random()*0.01} image={event.logo}/>
+                   <Poster key={event.id} yRotation={(Math.random()-0.1)*0.5} xPosition={(index-4)*0.4} yPosition={(Math.random()-0.5)*2} zPosition={(Math.random()*0.04)+0.1} image={event}/>
                  )
              })}
            </>
--- client/src/components/Poster.jsx
+++ client/src/components/Poster.jsx
@@ -1,10 +1,12 @@
 import { useLoader } from "@react-three/fiber"
+import { Text } from "@react-three/drei"
 import { BoxGeometry, MeshStandardMaterial, TextureLoader } from "three"
 
 
 function Poster({yRotation, xPosition,yPosition,zPosition, image}){
-    const posterMap = useLoader(TextureLoader, image.url)
-    const aspectRatio = image.original.height/image.original.width
+    const color = Math.random()*255
+    const posterMap = useLoader(TextureLoader, image.logo.url)
+    const aspectRatio = image.logo.original.height/image.logo.original.width
     let imageHeight = 0
     let imageWidth = 0
     if(aspectRatio > 0){
@@ -15,16 +17,97 @@ function Poster({yRotation, xPosition,yPosition,zPosition, image}){
         imageHeight = 0.3*aspectRatio
         imageWidth = 0.3
     }
+    const date = new Date(image.start.local)
     return(
-        <group>
-            <mesh rotation={[0,0,yRotation]} position={[xPosition,yPosition,zPosition-0.0001]}>
+        <group 
+        position={[xPosition,yPosition,zPosition]}
+        rotation={[0,0,yRotation]} 
+        >
+           <Text 
+          color={`hsl(${color}, 100%, 95%)`} 
+          anchorX="left" 
+          anchorY="top" 
+          fontSize="0.04"
+          fontWeight="bold"
+          overflowWrap="normal"
+          maxWidth={0.5}
+          position={[-0.28,0.44,0]}
+          >
+            {image.name.text}
+          </Text>
+          <Text 
+          color={`hsl(${color}, 100%, 10%)`} 
+          anchorX="left" 
+          anchorY="top" 
+          fontSize="0.02" 
+          fontWeight="bold"
+          overflowWrap="normal"
+          maxWidth={0.5}
+          position={[-0.28,0.24,0]}
+          >
+            {image.description.text}
+          </Text>
+          <Text 
+          color={`hsl(${color}, 100%, 90%)`} 
+          anchorX="left" 
+          anchorY="top" 
+          fontSize="0.06"
+          fontWeight="bold"
+          overflowWrap="normal"
+          maxWidth={0.5}
+          position={[-0.28,-0.35,0]}
+          >
+            {`£${image.price}`}
+          </Text>
+          <Text 
+          color={`hsl(${color}, 100%, 90%)`} 
+          anchorX="right" 
+          anchorY="top" 
+          fontSize="0.04" 
+          fontWeight="bold"
+          overflowWrap="normal"
+          maxWidth={0.5}
+          position={[0.28,-0.35,0]}
+          >
+            {date.toDateString()}
+          </Text>
+          <Text 
+          color={`hsl(${color}, 100%, 90%)`} 
+          anchorX="right" 
+          anchorY="top" 
+          fontSize="0.04" 
+          fontWeight="bold"
+          overflowWrap="normal"
+          maxWidth={0.5}
+          position={[0.28,-0.395,0]}
+          >
+            {date.toLocaleTimeString("en-US",{hour:"2-digit", minute: "2-digit"})}
+          </Text>
+            <mesh  
+            position={[0,0,-0.0015]}>
                             <planeGeometry args={[0.6,0.9,1]}/>
-                            <meshStandardMaterial color={"yellow"}/>
+                            <meshStandardMaterial color={`hsl(${color}, 50%, 70%)` }/>
+            </mesh>
+            <mesh  
+            position={[0,0.35,-0.001]}>
+                            <planeGeometry args={[0.6,0.2,1]}/>
+                            <meshStandardMaterial color={`hsl(${color}, 100%, 10%)` }/>
+            </mesh>
+            <mesh  
+            position={[0,-0.4,-0.001]}>
+                            <planeGeometry args={[0.6,0.1,1]}/>
+                            <meshStandardMaterial color={`hsl(${color}, 100%, 10%)` }/>
             </mesh>
             
-            <mesh rotation={[0,0,yRotation]} position={[xPosition,yPosition,zPosition]}>
+            <mesh  
+            position={[0,-0.1,0]}>
                             <planeGeometry args={[imageHeight, imageWidth]}/>
                             <meshStandardMaterial map={posterMap}/>
+            </mesh>
+            <mesh  
+            position={[0,-0.1,-0.001]}>
+                            <planeGeometry args={[imageHeight+0.03, imageWidth+0.03]}/>
+                            <meshStandardMaterial color={`hsl(${color}, 100%, 15%)`}/>
             </mesh>
 
         </group>
