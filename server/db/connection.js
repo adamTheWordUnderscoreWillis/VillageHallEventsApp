3db01ed3a4ae952b90babd7050c4798fef1e9e71
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2025-02-04 18:21:46 +0000

    Adds Client connect into connect to server

--- server/db/connection.js
+++ server/db/connection.js
@@ -11,13 +11,28 @@ const client = new MongoClient(uri, {
     }
   });
 
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect()
+      // Send a ping to confirm a successful connection
+      await client.db("development").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
 
   let database
   module.exports = {
     connectToServer: async ()=>{
-        database = await client.db("development")
+        await run().catch(console.dir);
+        await client.connect()
+        database = client.db("development")
       },
-      connectToTestServer: ()=>{
+      connectToTestServer: async ()=>{
+        await client.connect()
         database = client.db("test")
       },
       getDb: ()=> {
