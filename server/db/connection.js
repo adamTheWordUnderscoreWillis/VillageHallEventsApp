cda50f435492a6a5e2689cfc3b26dabab0f2151f
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2025-02-04 15:43:14 +0000

    added ping test back into connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -11,9 +11,23 @@ const client = new MongoClient(uri, {
     }
   });
 
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect()
+      // Send a ping to confirm a successful connection
+      await client.db("development").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+
   let database
   module.exports = {
-      connectToServer: ()=>{
+    connectToServer: ()=>{
+        run().catch(console.dir);
           database = client.db("development")
       },
       connectToTestServer: ()=>{
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const connect = require("./db/connection.js")
 const app = require("./app.js")
 
 const {PORT} = process.env
+
 app.listen(PORT,()=>{
     connect.connectToServer()
     console.log(`Server is listening on ${PORT}`)
