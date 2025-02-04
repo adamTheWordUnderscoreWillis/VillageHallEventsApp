b9109e2ed2d46df6992fde4f65c76b36417d65e0
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2025-02-04 20:06:42 +0000

    adds headers

--- server/app.js
+++ server/app.js
@@ -10,11 +10,11 @@ const app = express()
 
 app.use(cors());
 app.use(express.json());
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "little-tidford-village-hall.vercel.app"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next()
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "little-tidford-village-hall.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+  });
 // app.use(session({ 
 //     secret: process.env.SECRET,
 //     saveUninitialized: false, 
